894c307be0ad3c81e5a0c1477583f35ef8d2ef00
Mirco <zinga.90@hotmail.it>
2015-07-01 01:28:32 +0200

    Modificato per SavePictures.py
    gestione delle due nuove cartelle
    mask2
    depth_corr2

--- src/script.sh
+++ src/script.sh
@@ -1,23 +1,36 @@
 #!/bin/bash
-#script creato per ridurre il numero di file delle cartelle deph, mask e img
-#è un semplice script da terminale;
-#ricordo di lanciare il comando chmod a+x script.sh per rendere il programma eseguibile da console
+#script creato per ridurre del 30% le immagini gelle cartelle deph, mask e img
+#è un semplice script da console ricordo di lanciare il comando chmod a+x script.sh per rendere il programma eseguibile da console
 #le nuove immagni verrano salvate nelle cartelle deph2, img2 e mask2 per controlalre il risultato
 #configurazione
 i=1
-file=4
+file=8
 molt=3
-#crea le cartelle
+#reset cartelle di appoggio
+rm -r depth2 
+rm -r img2 
+rm -r mask2
+rm -r depth_corr2
 mkdir depth2
 mkdir img2
 mkdir mask2
-#60 Perché mi permette di al file 181.png
+mkdir depth_corr2
 for X in $(seq 60)
 do
 	cp depth/$file.png depth2/$i.png
 	cp img/$file.png img2/$i.png
 	cp mask/$file.png mask2/$i.png
+	cp depth_corr/$file.png depth_corr2/$i.png
 	let "file = file + molt"
 	let "i = i + 1"
 done
+rm -r depth
+rm -r img
+rm -r mask
+rm -r depth_corr
+mv depth2 depth
+mv img2 img
+mv mask2 mask 
+mv depth_corr2 depth_corr
 echo fine
+#consiglio di svuotare il cestino!!!
