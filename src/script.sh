6bb0cb7219517d2d82908156e47da56037b3766f
Mirco <zinga.90@hotmail.it>
2015-07-01 01:52:34 +0200

    Update script.sh per la vuova SavePictures.py
    P.s. il file deve essere configurato in base ai riusltati prodotti dalla programma SavePictures

--- src/script.sh
+++ src/script.sh
@@ -3,8 +3,12 @@
 #è un semplice script da console ricordo di lanciare il comando chmod a+x script.sh per rendere il programma eseguibile da console
 #le nuove immagni verrano salvate nelle cartelle deph2, img2 e mask2 per controlalre il risultato
 #configurazione
+#-- Il file deve essere configurato in base ai risultati prodotti dalla programma SavePictures
+#indice di partenza dei file selezionati primo file creato sarà 1.png
 i=1
+#primo file di partenza (dipende dal video) 
 file=8
+#skip (dipende dal video) 
 molt=3
 #reset cartelle di appoggio
 rm -r depth2 
@@ -15,6 +19,7 @@ mkdir depth2
 mkdir img2
 mkdir mask2
 mkdir depth_corr2
+#valore 60 permette di arrivare al file 181.png (1+60*3)[i+seq*mol]
 for X in $(seq 60)
 do
 	cp depth/$file.png depth2/$i.png
@@ -24,6 +29,8 @@ do
 	let "file = file + molt"
 	let "i = i + 1"
 done
+#riporto tutto nelle cartelle pricipali.
+#commentare questa parte se non si vuole lasciare i risultati nelle cartelle create nella prima parte
 rm -r depth
 rm -r img
 rm -r mask
@@ -32,5 +39,6 @@ mv depth2 depth
 mv img2 img
 mv mask2 mask 
 mv depth_corr2 depth_corr
-echo fine
 #consiglio di svuotare il cestino!!!
+echo fine
+
