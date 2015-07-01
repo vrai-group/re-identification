9d6c4c6059e8f69372864d2dcade848ee1785d3b
N1k06 <nikof.06@gmail.com>
2015-07-01 03:11:16 +0200

    Update DepthToMesh.py
    Modifica del metodo che converte la depth map in nuvola di punti. Si è corretto il problema della versione precedente per cui nella nuvola di punti restavano alcuni elementi residui con coordinate (0,0,0).

--- src/DepthToMesh.py
+++ src/DepthToMesh.py
@@ -13,18 +13,33 @@ EXT1 = ".png"
 EXT2 = ".ply"
 EXT3 = ".pcd"
 
+#TODO: trovare, se possibile, un modo più efficiente di effettuare la conversione
+#invece dei doppi cicli while.
 def cvtDepthTo3DPoints(m, scale_factor):
 	H, W = m.shape
-	nrows = cv2.countNonZero(m)
+	nrows = 0
+	
+	#cv2.findNonZero(m) restituisce un valore diverso dal risultato di questo for,
+	#quindi non può essere usato. 
+	for i in range(1,W):
+		for j in range(1,H):
+			if m[j-1][i-1] > 0:
+				nrows+= 1
+	#inizializzazione di un vettore di punti in 3D.			
 	points_array = np.zeros((nrows,3)).astype(np.float32)
+	
+	#ogni pixel del depth frame viene convertito in un punto in 3d, dove 
+	#la coordinata in x è data dall'indice i, quella in y dall'indice j,
+	#e quella in z dal valore di profondità.
 	k = 0
 	for i in range(1,W):
 		for j in range(1,H):
-			if m[j-1][i-1] != 0:
+			if m[j-1][i-1] > 0:
 				points_array[k][0] = i
 				points_array[k][1] = j
-				points_array[k][2] = m[j-1][i-1].astype(np.float32)/scale_factor
+				points_array[k][2] = m[j-1][i-1]/scale_factor	
 				k+= 1
+
 	return points_array
 
 def main():
@@ -49,7 +64,7 @@ def main():
 		pcl.save(pointcloud, str(i) + EXT2)
 		pointcloud.to_file(str(i) + EXT3)
 		
-		print "Converting " + str(i) + ".png" + "into " + str(i) + ".pcd"
+		print "Converting " + str(i) + ".png" + " into " + str(i) + ".pcd and into " + str(i) + ".ply"
 		os.chdir("..")
 		
 		i+=1
